9da9bbed2fd22a4494589519d35f6730b085aae6
GREENGANGA <GreeN11.dev@gmail.com>
2024-08-29 17:16:04 +0530

    4

---
+++ Button-ripple-effect/index.js
@@ -0,0 +1,10 @@
+//get the btn class using queryselector meyhod
+const btnEl = document.querySelector('.btn');
+
+btnEl.addEventListener("mouseover",(event)=>{
+    const x = event.pageX-btnEl.offsetLeft;
+    const y = event.pageY-btnEl.offsetTop;
+
+    btnEl.style.setProperty("--xPos", x +'px');
+    btnEl.style.setProperty("--yPos", y +"px");
+})
