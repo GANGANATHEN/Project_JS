f72f28e86bf5b614145e82b45f1c6b7ddfdf54a8
GREENGANGA <GreeN11.dev@gmail.com>
2024-09-14 13:42:51 +0530

    13

---
+++ Rotating-image-Gallery/script.js
@@ -0,0 +1,27 @@
+const imagecontainerEl = document.querySelector(".image-container")
+const prevEl = document.getElementById("prev")
+const nextEl = document.getElementById("next")
+
+let x = 0;
+let timer;
+
+prevEl.addEventListener("click",()=>{
+    x+=45;
+    clearTimeout(timer);
+    updateGallery();
+})
+
+nextEl.addEventListener("click",()=>{
+    x-=45;
+    clearTimeout(timer);
+    updateGallery();
+})
+
+function updateGallery(){
+    imagecontainerEl.style.transform = `perspective(1000px) rotateY(${x}deg)`
+    timer = setTimeout(()=>{
+        x-=45;
+        updateGallery();
+    },2000)
+}
+updateGallery();
